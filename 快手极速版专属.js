fa991b60ed12ff2fb85ba66caa5293e946b6c467
Vincent <985126792@qq.com>
2020-12-31 10:14:03 +0800

    修改进入福利页面，优化领广告福利，优化直播福利

--- 快手极速版专属.js
+++ 快手极速版专属.js
@@ -7,52 +7,66 @@
  * 请勿用于非法用途！
 */
 
+/**快手极速版新页面 */
+
+/**-------------------------打开APP-------------------------------- */
 auto.waitFor()
-var target;
 
 let times = rawInput("请输入要自动刷的视频次数：", "500")  //用户设置刷视频的个数，默认100
 launchApp("快手极速版");
 console.show()  //显示悬浮窗（需要先打开悬浮窗权限）
-sleep(10000)    //等待应用打开
+sleep(5000)    //等待应用打开
 console.log("准备就绪！")
+
+/**-------------------------打开福利页面-------------------------------- */
 
 id("left_btn").click()//点击菜单
 dytimes(2000, 4000)
-drawingOrder("2").id("container").click()// drawingOrder("2") 点击去赚钱
+drawingOrder("3").id("container").click()// drawingOrder("2") 点击去赚钱
 dytimes(2000, 4000)
 
-welfare();
-liveds();
-videos();
-
+/**-------------------------调用方法-------------------------------- */
+welfare()
+dytimes(1000, 3000)
+liveds()
+dytimes(1000, 3000)
+videos()
+dytimes(1000, 3000)
 console.hide()
 home();//回到首页
+/**-------------------------10次福利金币-------------------------------- */
 
-/**-------------------------10次福利金币（广告福利最近一直没反应可能又开始收割了，随缘吧）-------------------------------- */
 function welfare() {
     swipe(device.width / 2, device.height * (8 / 9), device.width / 2, device.height * (1 / 2), 150)
     dytimes(3000, 5000);
-    for (let i = 1; i <= 10; i++) {
+    var i = 1
+    while (true) {
+        i = i + 1;
         if (text("福利 领金币").findOnce()) {
             text("福利 领金币").click();
             console.log("第" + i + "次广告福利")
             //等待广告关闭按钮点击
+            dytimes(3000, 5000);
             id("video_close_icon").waitFor()
             id("video_close_icon").click()
             dytimes(3000, 5000);
-        } else if (text("福利").findOnce()) {
+        }
+        if (text("福利").findOnce()) {
             text("福利").click();
             console.log("第" + i + "次广告福利")
             //等待广告关闭按钮点击
+            dytimes(3000, 5000);
             id("video_close_icon").waitFor()
             id("video_close_icon").click()
             dytimes(3000, 5000);
-        } else if (text("明日再来").findOnce()){
-            console.log("10次广告福利结束啦！");
-            break
+        }
+        if (className("android.view.View").text("明天看视频继续领取1000金币").exists() || i == 20) {
+            console.log("10次广告福利结束啦！" + i);
+            break;
         }
     }
 }
+
 
 /**-------------------------10次直播金币-------------------------------- */
 function liveds() {
@@ -64,8 +78,9 @@ function liveds() {
         back()
     }
     else {
+        className("android.view.View").text("直播送礼赚金币").findOne().parent().click()
         text("看直播").click()
-        for (let i = 1; i <= 2; i++) {
+        for (let i = 1; i <= 10; i++) {
             console.log("第" + i + "次直播奖励")
             sleep(32 * 1000)
             swipe(device.width / 2, device.height * (8 / 9), device.width / 2, device.height * (1 / 4), 150)
@@ -73,15 +88,17 @@ function liveds() {
                 text("close_btn").click()
                 console.log("有关注主播弹出");
                 // id("close_btn").waitFor()
-            } else if (i == 2 || text("退出").exists()) {
+            } else if (i == 10) {
                 console.log("10次直播福利结束啦！里面");
                 back();
-                dytimes(1000, 3000);
-               //可能随机出现关注主播，视情况修改
-               //textContains("退出").click()
                 back();
-                dytimes(1000, 3000);
                 back();
+                if (textContains("退出").exists()) {
+                    textContains("退出").click()
+                    back();
+                }
+
+                
             }
         }
     }
@@ -94,9 +111,12 @@ function videos() {
         u = i + 1
         console.log("总任务量：" + times + ";已完成：" + u)
         /*随机回看 */
-        j = random(1, 30)
-        if (j == 1) {
+        j = random(1, 20)
+        if (j == 3) {
             lookBack()
+        }
+        if (j == 20) {
+            nextTwo()
         }
     }
 }
@@ -129,6 +149,7 @@ function dytimes(time1, time2) {
     delayTime = random(time1, time2)
     sleep(delayTime)    //在视频停留8-12秒
 }
+
 
 /**使用本脚本的同学请填写邀请码：
     * 抖音极速版邀请码：87429262 （推荐）
